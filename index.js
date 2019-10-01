d0fee7411fc2aef10abe894a4eed299cbcc3a53f
Temirlan21 <zhumabaevtemir.lan2112@gmail.com>
2019-10-01 12:31:39 +0600

    First

---
+++ index.js
@@ -0,0 +1,13 @@
+
+   let input = prompt("Ведите любимое число")
+    inputNum = parseInt(input, 10) {    
+
+     if (isNaN(inputNum) || inputNum < 1)
+    alert("Вы ввели не правильное число")
+    
+    } else {
+    let a (input * input)-1)%24;
+    if (input === 1 || input === 2 || input === 5 || input === 25);
+    else if 
+}
+
